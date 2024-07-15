eb525d97e1a441c9f49af04f1b7eab77c127947c
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-15 16:38:37 -0600

    feat(InputForm): implement the component form and input fields initial layout

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,5 +1,28 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <form action="">
+    <div class="form-container">
+      <div>
+        <label for="total">Total</label>
+        <input type="number" required />
+      </div>
+
+      <div>
+        <label for="tip">Tip %</label>
+        <input type="number" required />
+      </div>
+
+      <div>
+        <label for="people">People</label>
+        <input type="number" required />
+      </div>
+
+      <div>
+        <input type="submit" value="Calculate" />
+      </div>
+    </div>
+  </form>
+</template>
 
 <style scoped></style>
