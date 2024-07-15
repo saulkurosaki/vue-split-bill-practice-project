17a696d44f6053c6b97718b0270d08305e73e787
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-15 16:58:41 -0600

    feat(Label): implement the Label component initial layout

--- src/components/PeopleView.vue
+++ src/components/PeopleView.vue
@@ -1,9 +1,25 @@
 <script setup>
-import { store } from "../store/store";
+import { getGrandTotal, store } from "../store/store";
 </script>
 
 <template>
   <div class="no-items" v-if="store.people.length === 0">No items</div>
+
+  <div class="people-view" v-if="store.people.length > 0">
+    <div class="header">
+      <div>
+        <div>
+          {{ getGrandTotal() }}
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {{ store.params.remaining }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
