613c3c090e998a5985d517236741410ffa4000c0
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-15 16:50:01 -0600

    feat(PeopleView): implement the ternary display of the no-items div if store.people.length is equal to 0

--- src/components/PeopleView.vue
+++ src/components/PeopleView.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import { store } from "../store/store";
+</script>
 
-<template></template>
+<template>
+  <div class="no-items" v-if="store.people.length === 0">No items</div>
+</template>
 
 <style scoped></style>
