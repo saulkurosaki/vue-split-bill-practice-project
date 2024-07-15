77f3f98da5a7d14de3913fb9880ac23c045a3c03
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-15 16:35:35 -0600

    feat(SplitBillView): implement the InputForm and PeopleView components within the main component

--- src/components/SplitBillView.vue
+++ src/components/SplitBillView.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import InputForm from "./InputForm.vue";
+import PeopleView from "./PeopleView.vue";
+</script>
 
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <InputForm />
+    <PeopleView />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
