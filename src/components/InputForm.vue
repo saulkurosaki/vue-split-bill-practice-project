8f085cf20e7447a3ca7ecd83878e94b9fe2988a5
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-15 16:41:04 -0600

    feat(InputForm): implement the tracking state for each input field

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,21 +1,27 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const total = ref(0);
+const tip = ref(0);
+const people = ref(0);
+</script>
 
 <template>
   <form action="">
     <div class="form-container">
       <div>
         <label for="total">Total</label>
-        <input type="number" required />
+        <input type="number" v-model="total" required />
       </div>
 
       <div>
         <label for="tip">Tip %</label>
-        <input type="number" required />
+        <input type="number" v-model="tip" required />
       </div>
 
       <div>
         <label for="people">People</label>
-        <input type="number" required />
+        <input type="number" v-model="people" required />
       </div>
 
       <div>
