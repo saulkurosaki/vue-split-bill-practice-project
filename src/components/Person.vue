d08be3fae62acfc6ac0addd3371211d53027b0c2
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 11:13:19 -0600

    feat(Person): implement the children component props to accept from the parent along the paid ref state

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+import { defineProps, ref } from "vue";
 
-<template></template>
+const props = defineProps(["id", "numberOfPerson", "totalPerPerson", "paid"]);
+const paid = ref(false);
+</script>
+
+<template>
+  <div>
+    <div class="person-number"></div>
+    <div class="person-to-pay"></div>
+    <div class="paid"></div>
+  </div>
+</template>
 
 <style scoped></style>
