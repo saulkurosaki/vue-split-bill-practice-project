53de73104f47de7ce0d72a7c6df0d006a1e64dad
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-16 11:18:28 -0600

    feat(Person): implement the handleChange function along the display of each division

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,15 +1,31 @@
 <script setup>
 import { defineProps, ref } from "vue";
+import { pay } from "../store/store.js";
 
 const props = defineProps(["id", "numberOfPerson", "totalPerPerson", "paid"]);
 const paid = ref(false);
+
+const handleChange = (e) => {
+  paid = e.target.value;
+
+  pay(props.id, paid);
+};
 </script>
 
 <template>
   <div>
-    <div class="person-number"></div>
-    <div class="person-to-pay"></div>
-    <div class="paid"></div>
+    <div class="person-number">Person {{ props.numberOfPerson }}</div>
+    <div class="person-to-pay">
+      {{
+        new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(props.totalPerPerson)
+      }}
+    </div>
+    <div class="paid">
+      <input type="checkbox" name="" id="" @change="handleChange" /> Paid
+    </div>
   </div>
 </template>
 
